ce5655cc7f502dbf1d2202d44acba5f7d6939125
Jan de Vries <jandevries@microsoft.com>
2022-05-25 15:39:02 +0200

    Showing data from Dataverse API

---
+++ src/MyApp/Models/Dataverse/Accounts.cs
@@ -0,0 +1,23 @@
+﻿namespace MyApp.Models.Dataverse
+{
+    internal class Accounts
+    {
+        public class WithRevenue
+        {
+            public class Rootobject
+            {
+                public string odatacontext { get; set; }
+                public Value[] value { get; set; }
+            }
+
+            public class Value
+            {
+                public string odataetag { get; set; }
+                public string name { get; set; }
+                public float revenue { get; set; }
+                public string _transactioncurrencyid_value { get; set; }
+                public string accountid { get; set; }
+            }
+        }
+    }
+}
